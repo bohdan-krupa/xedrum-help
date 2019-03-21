6d3a45e17f0405f2951c123720ef0f1a1d660ccf
Bodya <bodya.save.dev@gmail.com>
2019-03-21 22:52:53 +0200

    update

--- src/js/MyHeader.js
+++ src/js/MyHeader.js
@@ -2,26 +2,30 @@ Vue.component('my-header', {
 	template: `<h1>Xedrum Help</h1>`
 })
 
+Vue.component('profile', {
+	data: () => {
+		return {
+			skills: [
+				'НЛП',
+				'Робота з травмами',
+				'Курс стандартної психології',
+				'Курс метафоричних карт',
+				'Бутійність',
+				'Курси по скайпу'
+			]
+		}
+	},
+	template: `<div class="profile-container">
+		<div class="avatar"></div>
+		<li>
+			<ul v-for="skill in skills">{{ skill }}</ul>
+		</li>
+	</div>`
+})
+
 Vue.component('my-footer', {
 	template: `<footer>
 		<p>Copyrigh © 2018-2019&ensp;&ensp;&ensp;</p>
 		<a href="https://bohdan-krupa.github.io" target="_blank">Created by Bohdan Krupa</a>
 	</footer>`
-})
-
-let ThingsContainer = Vue.component('things-container', {
-	data: () => {
-		return {
-			active: 'shoes'
-		}
-	},
-	template: `<div>
-		<filter-container @viewActive="onViewActive"></filter-container>
-		<things :active="active"></things>
-	</div>`,
-	methods: {
-		onViewActive(tag) {
-			this.active = tag
-		}
-	}
 })
